3a8d1dee77bbc6c2f2e007fb977e603d1f8493b0
analuisatchobnian <ana.dantas@sptech.school>
2026-06-08 11:59:35 -0300

    mes da kpi 5 esta vindo em pt-br

--- src/models/dashboardModel.js
+++ src/models/dashboardModel.js
@@ -85,7 +85,20 @@ return database.executar(instrucao);
 function kpiPiorMes(idCliente) {
 var instrucao = `
 SELECT
-MONTHNAME(c.data_horario_medicao) AS nomeMes,
+CASE MONTH(c.data_horario_medicao)
+  WHEN 1 THEN 'Janeiro'
+  WHEN 2 THEN 'Fevereiro'
+  WHEN 3 THEN 'Março'
+  WHEN 4 THEN 'Abril'
+  WHEN 5 THEN 'Maio'
+  WHEN 6 THEN 'Junho'
+  WHEN 7 THEN 'Julho'
+  WHEN 8 THEN 'Agosto'
+  WHEN 9 THEN 'Setembro'
+  WHEN 10 THEN 'Outubro'
+  WHEN 11 THEN 'Novembro'
+  WHEN 12 THEN 'Dezembro'
+END AS nomeMes,
 AVG(c.valor) AS mediaUmidade
 FROM captura c
 JOIN sensor s ON c.fkSensor = s.id
