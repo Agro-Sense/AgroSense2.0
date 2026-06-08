fd0bf560da9ed488fb941adaa7598a69f86b06ee
analuisatchobnian <ana.dantas@sptech.school>
2026-06-08 11:52:04 -0300

    mes da kpi 4 esta vindo em portugues

--- src/database/script-tabelas.sql
+++ src/database/script-tabelas.sql
@@ -174,7 +174,20 @@ SELECT p.fkCliente, s.id
 CREATE VIEW vw_kpi4 AS
 SELECT
 p.fkCliente,
-MONTHNAME(c.data_horario_medicao) AS nomeMes,
+CASE MONTH(c.data_horario_medicao)
+  WHEN 1 THEN 'Janeiro'
+  WHEN 2 THEN 'Fevereiro'
+  WHEN 3 THEN 'Março'
+  WHEN 4 THEN 'Abril'
+  WHEN 5 THEN 'Maio'
+  WHEN 6 THEN 'Junho'
+  WHEN 7 THEN 'Julho'
+  WHEN 8 THEN 'Agosto'
+  WHEN 9 THEN 'Setembro'
+  WHEN 10 THEN 'Outubro'
+  WHEN 11 THEN 'Novembro'
+  WHEN 12 THEN 'Dezembro'
+END AS nomeMes,
 AVG(c.valor) AS mediaUmidade
 FROM captura c
 JOIN sensor s ON c.fkSensor = s.id
@@ -188,7 +201,20 @@ SELECT * FROM vw_kpi4;
 -- select da kpi5
 CREATE VIEW vw_kpi5 AS
 SELECT
-MONTHNAME(c.data_horario_medicao) AS nomeMes,
+CASE MONTH(c.data_horario_medicao)
+  WHEN 1 THEN 'Janeiro'
+  WHEN 2 THEN 'Fevereiro'
+  WHEN 3 THEN 'Março'
+  WHEN 4 THEN 'Abril'
+  WHEN 5 THEN 'Maio'
+  WHEN 6 THEN 'Junho'
+  WHEN 7 THEN 'Julho'
+  WHEN 8 THEN 'Agosto'
+  WHEN 9 THEN 'Setembro'
+  WHEN 10 THEN 'Outubro'
+  WHEN 11 THEN 'Novembro'
+  WHEN 12 THEN 'Dezembro'
+END AS nomeMes,
 AVG(c.valor) AS mediaUmidade
 FROM captura c
 JOIN sensor s ON c.fkSensor = s.id
@@ -196,5 +222,4 @@ JOIN plantacao p ON s.fkPlantacao = p.id
 GROUP BY nomeMes
 ORDER BY mediaUmidade DESC
 LIMIT 1;
-SELECT * FROM vw_kpi5;
 
